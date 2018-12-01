9cbbc05b08d27ea9b0d7248e8301db905990a1ac
Jonathan Coulthard <Jon@jonathans-mbp-4.home>
2018-12-01 14:25:41 +0000

    uncommented naive solution

--- day_1.m
+++ day_1.m
@@ -1,26 +1,26 @@
 clear; clc; close all;
 %% Naive solution - requires looping many times - takes about 0.25s on my machine
-% frequencies = load('day_1_input.txt');
-% 
-% net_frequency_change = sum(frequencies)
-% 
-% previous_frequencies = [0];
-% curr_freq = 0;
-% 
-% found_repeat = false();
-% loop_number = 1;
-% 
-% while ~found_repeat
-%     curr_frequencies = previous_frequencies(end) + cumsum(frequencies);
-%     indices = ismember(curr_frequencies, previous_frequencies);
-%     
-%     if any(indices)
-%         disp(['Found frequency ' num2str(curr_frequencies(indices)') ' at positions ' num2str(find(indices)') char(10) ' on loop number ' num2str(loop_number)]);
-%         found_repeat = true();
-%     end;
-%     previous_frequencies = [previous_frequencies; curr_frequencies];
-%     loop_number = loop_number+1;
-% end;
+frequencies = load('day_1_input.txt');
+
+net_frequency_change = sum(frequencies)
+
+previous_frequencies = [0];
+curr_freq = 0;
+
+found_repeat = false();
+loop_number = 1;
+
+while ~found_repeat
+    curr_frequencies = previous_frequencies(end) + cumsum(frequencies);
+    indices = ismember(curr_frequencies, previous_frequencies);
+    
+    if any(indices)
+        disp(['Found frequency ' num2str(curr_frequencies(indices)') ' at positions ' num2str(find(indices)') char(10) ' on loop number ' num2str(loop_number)]);
+        found_repeat = true();
+    end;
+    previous_frequencies = [previous_frequencies; curr_frequencies];
+    loop_number = loop_number+1;
+end;
 
 %% Cleverer solution based on modular arithmetic - takes about 0.02s on my machine
 
