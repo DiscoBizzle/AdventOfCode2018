51243a0a39f762b844e56f77007368eeeb31e731
Jonathan Coulthard <Jon@Jonathans-MacBook-Pro-4.local>
2018-12-03 17:10:42 +0000

    Doing Day 3 part 2 in a non-dumb way

--- day_3.m
+++ day_3.m
@@ -3,27 +3,31 @@ clear; clc; close all;
 Inputs = importdata('day_3_input.txt');
 
 Grid = zeros(1001, 1001);
+% Grid = spalloc(1001, 1001, 4000);
 
 for j = 1:length(Inputs)
     % Inputs are e.g. #1 @ 817,273: 26x26
     CurrInput  = strsplit(Inputs{j} ,{'\s', '#', '@', ',', ':', 'x'}, 'DelimiterType','RegularExpression');
-    CurrID = str2num(CurrInput{2});
-    X = str2num(CurrInput{3})+1;
-    Y = str2num(CurrInput{4})+1;
-    W = str2num(CurrInput{5})-1;
-    H = str2num(CurrInput{6})-1;
-    
-    if ((X == 308) && (Y == 185))
-        disp(CurrID) % Solution to the second part
-    end;
+    CurrID = str2double(CurrInput{2});
+    X = str2double(CurrInput{3})+1;
+    Y = str2double(CurrInput{4})+1;
+    W = str2double(CurrInput{5})-1;
+    H = str2double(CurrInput{6})-1;
     
     Grid(X:(X+W), Y:(Y+H)) = Grid(X:(X+W), Y:(Y+H)) + 1;
 end;
 
 OverlappingCells = sum(sum(Grid > 1)) % Solution to the first part
 
-% Plot the grid in a form where it is easy to see where the non-overlapping rectangle is
-% Visually, the non-overlapping grid square is at 308, 185
-NewGrid = (Grid > 1)*10 + (Grid == 1);
-contourf(NewGrid)
-
+for j = 1:length(Inputs)
+    % Inputs are e.g. #1 @ 817,273: 26x26
+    CurrInput  = strsplit(Inputs{j} ,{'\s', '#', '@', ',', ':', 'x'}, 'DelimiterType','RegularExpression');
+    CurrID = str2double(CurrInput{2});
+    X = str2double(CurrInput{3})+1;
+    Y = str2double(CurrInput{4})+1;
+    W = str2double(CurrInput{5})-1;
+    H = str2double(CurrInput{6})-1;
+    if all(all( Grid(X:(X+W), Y:(Y+H)) == 1))
+        disp(['Non overlapping square is ' num2str(CurrID)]);
+    end;
+end;
